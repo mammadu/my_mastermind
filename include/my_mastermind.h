64bd5874a3c623705700c1a48431905b4f4e45a2
mammadu <mammadudiallo@gmail.com>
2023-11-10 13:41:15 -0800

    created random code generator added my_mastermind.h create function 'create_secret_code'

---
+++ include/my_mastermind.h
@@ -0,0 +1,13 @@
+#ifndef my_mastermind_H  // Include guards to prevent multiple inclusion
+#define my_mastermind_H
+
+// define constants
+#define SECRET_CODE_LENGTH 4
+
+// Function declaration
+void create_secret_code(int *secret_code);
+
+// Variable declaration
+
+
+#endif  // End of include guards
---
+++ src/my_mastermind.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "../include/my_mastermind.h"
+
+int main (){
+    
+    
+    int secret_code_array[SECRET_CODE_LENGTH];
+    create_secret_code(secret_code_array);
+
+    for (int i = 0; i < SECRET_CODE_LENGTH; i++)
+    {
+        printf("%d\n", secret_code_array[i]);
+    }
+}
+
+void create_secret_code(int *secret_code){
+    // Seed the random number generator with the current time
+    srand(time(NULL));
+
+    for (int i = 0; i < 4; i++)
+    {
+        secret_code[i] = rand() % 9;
+    }
+}
