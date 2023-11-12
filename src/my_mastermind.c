555edac61612edfdd0e88f45005101a42c033dbf
mammadu <mammadudiallo@gmail.com>
2023-11-12 14:46:57 -0800

    made a struct for holding information about flags

--- src/my_mastermind.c
+++ src/my_mastermind.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 
 #include "../include/my_mastermind.h"
+
+typedef struct{
+    char flag_secret_code_representation;
+    bool flag_secret_code_on;
+    int flag_secret_code_position;
+    char flag_attempts_representation;
+    bool flag_attempts_on;
+    int flag_attempts_position;
+}flag_struct;
+
+flag_struct init_flag_struct(){
+    flag_struct flag;
+    flag.flag_secret_code_representation = 'c';
+    flag.flag_secret_code_on = false;
+    flag.flag_attempts_representation = 't';
+    flag.flag_attempts_on = false;
+    return flag;
+}
+
 
 int main (){
     int secret_code_array[SECRET_CODE_LENGTH];
